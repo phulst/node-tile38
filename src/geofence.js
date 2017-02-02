f3b7b8332f0cc5926ecea1aea14c008abdcf308d
Peter Hulst <peter.hulst@springshot.com>
2017-02-02 00:19:59 -0800

    more progress on geofence class

--- src/geofence.js
+++ src/geofence.js
@@ -1,8 +1,17 @@
 
 // represents a GeoFence in Tile38
 class Geofence {
+    
     /**
-     * detect: array of
+     * The following properties may be passed:
+     *
+     * command: (del,drop,set) (array, or string with single or comma separated values)
+     * detect: (inside,outside,enter,exit,cross) (array, or string with single or comma separated values)
+     * output: (count|ids|objects|points|bounds|hashes N) specify one of these values (hashes should be followed by precision)
+     *
+     * one of the following properties must be set:
+     * getObject: [key, id]   to identify any object already in database
+     * 
      * @param opts
      */
     constructor(opts) {
@@ -14,44 +23,98 @@ class Geofence {
     
     // returns an array of commands and options to be sent as part of the command string. 
     commands() {
+
         let cmd = [];
         if (this.options.detect) {
+            let values = this.assertValues(this.options.detect, 'options.detect',
+                ['inside','outside','enter','exit','cross'], true);
             cmd.push('DETECT');
-            cmd.push(this.options.detect.join(','));
+            cmd.push(values.join(','));
         }
 
         if (this.options.commands) {
+            let commands = this.assertValues(this.options.commands, 'options.commands',
+                ['del','set','drop'], true);
             cmd.push('COMMANDS');
-            cmd.push(this.options.commands.join(','));
+            cmd.push(commands.join(','));
         }
 
-        if (this.options.get) {
+        // output must be a single value
+        if (this.options.output) {
+            cmd.push(this.options.output.toUpperCase());
+            if (this.options.output == 'hashes') {
+                // add Hash precision if HASHES used
+                cmd.push(this.options.hashPrecision);
+            }
+        }
+
+        if (this.options.getObject) {
             cmd.push('GET');
-            val = this.options.get;
-            cmd.push(val.key);
-            cmd.push(val.id);
+            let val = this.options.getObject;
+            cmd.push(val[0]);
+            cmd.push(val[1]);
         }
 
+        if (this.options.bounds) {
+            cmd.push('BOUNDS');
+            let val = this.options.bounds;
+            for (let k of val) { // we should have 4 values here.
+                cmd.push(k);
+            }
+        }
+
+        if (this.options.object) {
+            cmd.push('OBJECT');
+            let val = this.options.object;
+            cmd.push(JSON.stringify(val));
+        }
+
+        if (this.options.tile) {
+            cmd.push('TILE');
+            let val = this.options.tile;
+            for (let k of val) { // we should have 3 values: x,y,z
+                cmd.push(k);
+            }
+        }
+
+        if (this.options.quadkey) {
+            cmd.push('QUADKEY');
+            cmd.push(this.options.quadkey);
+        }
+
+        if (this.options.hash) {
+            cmd.push('HASH');
+            cmd.push(this.options.hash);
+        }
 
         return cmd;
     }
 
-    validateOptions(opts) {
-        if (this.options.detect) {
-            this.assertArrayWithValues(this.options.detect, 'options.detect', ['inside','outside','enter','exit','cross']);
-        }
-        if (this.options.commands) {
-            this.assertArrayWithValues(this.options.commands, 'options.commands', ['del','set','drop']);
-        }
+    validateOptions() {
+        // if (this.options.detect) {
+        //     this.assertArrayWithValues(this.options.detect, 'options.detect', ['inside','outside','enter','exit','cross'], true);
+        // }
+        // if (this.options.commands) {
+        //     this.assertArrayWithValues(this.options.commands, 'options.commands', ['del','set','drop'], true);
+        // }
     }
 
-    assertArrayWithValues(arr, name, expectedValues) {
+    
+    assertValues(arr, name, expectedValues, multiple) {
+        let values;
         if (!Array.isArray(arr)) {
-            throw new Error(`${name} is not an array`);
+            values = arr.split(',');
+        } else {
+            values = arr;
         }
+        if (!multiple && values.length > 0) {
+            let msg = `${name} may only have one of the following values: ${expectedValues.join(',')}`;
+            throw new Error(msg);
+        }
+        return values;
         // TODO: check that all values in arr exist in expectedValues
-
     }
+
 }
 
 module.exports = Geofence;
--- test/test_geofence.js
+++ test/test_geofence.js
@@ -20,25 +20,171 @@ describe('geofence', function() {
 
 
     describe('detect', function() {
-        it("should parse DETECT option correctly", function(done) {
+        it("should parse DETECT option correctly when passed as array", function(done) {
             let opts = { detect: ['inside', 'outside'] };
             let geofence = new Geofence(opts);
             let cmd = geofence.commands();
-            console.log(cmd.join(' '));
             expect(cmd[0]).to.equal('DETECT');
             expect(cmd[1]).to.equal('inside,outside');
+            done();
+        });
+        it("should parse DETECT option correctly when passed as csv", function(done) {
+            let opts = { detect: 'inside,enter' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd[0]).to.equal('DETECT');
+            expect(cmd[1]).to.equal('inside,enter');
+            done();
+        });
+        it("should parse DETECT option correctly when passed as single value", function(done) {
+            let opts = { detect: 'cross' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd[0]).to.equal('DETECT');
+            expect(cmd[1]).to.equal('cross');
             done();
         });
     });
 
     describe('commands', function() {
-        it("should parse COMMANDS option correctly", function(done) {
-            let opts = { commands: ['set'] };
+        it("should parse COMMANDS option correctly when passed as array", function(done) {
+            let opts = { commands: ['set','del'] };
             let geofence = new Geofence(opts);
             let cmd = geofence.commands();
-            console.log(cmd.join(' '));
             expect(cmd[0]).to.equal('COMMANDS');
-            expect(cmd[1]).to.equal('set');
+            expect(cmd[1]).to.equal('set,del');
+            done();
+        });
+        it("should parse COMMANDS option correctly when passed as csv", function(done) {
+            let opts = { commands: 'del,drop' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd[0]).to.equal('COMMANDS');
+            expect(cmd[1]).to.equal('del,drop');
+            done();
+        });
+        it("should parse COMMANDS option correctly when passed as single value", function(done) {
+            let opts = { commands: 'del' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd[0]).to.equal('COMMANDS');
+            expect(cmd[1]).to.equal('del');
+            done();
+        });
+    });
+
+    describe('output', function() {
+        it("should set the COUNTS output", function(done) {
+            let opts = { output: 'counts' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('COUNTS');
+            done();
+        });
+        it("should set the IDS output", function(done) {
+            let opts = { output: 'ids' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('IDS');
+            done();
+        });
+        it("should set the OBJECTS output", function(done) {
+            let opts = { output: 'objects' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('OBJECTS');
+            done();
+        });
+        it("should set the POINTS output", function(done) {
+            let opts = { output: 'points' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('POINTS');
+            done();
+        });
+        it("should set the BOUNDS output", function(done) {
+            let opts = { output: 'bounds' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('BOUNDS');
+            done();
+        });
+        it("should set the HASHES output", function(done) {
+            let opts = { output: 'hashes 10' };
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(1);
+            expect(cmd[0]).to.equal('HASHES 10');
+            done();
+        });
+    });
+
+    describe('area formats', function() {
+        it("should set the GET format", function (done) {
+            let opts = {getObject: ['someObject', 'someKey']};
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(3);
+            expect(cmd[0]).to.equal('GET');
+            expect(cmd[1]).to.equal('someObject');
+            expect(cmd[2]).to.equal('someKey');
+            done();
+        });
+        it("should set the BOUNDS format", function (done) {
+            let b = [33.462, -112.268, 33.491, -112.245];
+            let opts = {bounds: b};
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(5);
+            expect(cmd[0]).to.equal('BOUNDS');
+            expect(cmd[1]).to.equal(b[0]);
+            expect(cmd[2]).to.equal(b[1]);
+            expect(cmd[3]).to.equal(b[2]);
+            expect(cmd[4]).to.equal(b[3]);
+            done();
+        });
+        it("should set the OBJECT format", function (done) {
+            let polygon = {"type":"Polygon","coordinates":
+                [[[-111.9787,33.4411],[-111.8902,33.4377],[-111.8950,33.2892],[-111.9739,33.2932],[-111.9787,33.4411]]]};
+            let opts = {object: polygon};
+            let geofence = new Geofence(opts);
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(2);
+            expect(cmd[0]).to.equal('OBJECT');
+            let exp = '{"type":"Polygon","coordinates":[[[-111.9787,33.4411],[-111.8902,33.4377],[-111.895,33.2892],[-111.9739,33.2932],[-111.9787,33.4411]]]}';
+            expect(cmd[1]).to.equal(exp);
+            done();
+        });
+        it("should set the TILE format", function (done) {
+            let tile = [ 10.0, 10.1, 10.2];
+            let geofence = new Geofence({ tile });
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(4);
+            expect(cmd[0]).to.equal('TILE');
+            expect(cmd[1]).to.equal(tile[0]);
+            expect(cmd[2]).to.equal(tile[1]);
+            expect(cmd[3]).to.equal(tile[2]);
+            done();
+        });
+        it("should set the QUADKEY format", function (done) {
+            let geofence = new Geofence({ quadkey: '32112321' });
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(2);
+            expect(cmd[0]).to.equal('QUADKEY');
+            expect(cmd[1]).to.equal(cmd[1]);
+            done();
+        });
+        it("should set the HASH format", function (done) {
+            let geofence = new Geofence({ hash: 'ab42e6' });
+            let cmd = geofence.commands();
+            expect(cmd.length).to.equal(2);
+            expect(cmd[0]).to.equal('HASH');
+            expect(cmd[1]).to.equal('ab42e6');
             done();
         });
     });
