e7167f4bd26f22cd54f7a0ef351fad8d22461aa9
Peter Hulst <peter.hulst@springshot.com>
2017-02-06 12:01:17 -0800

    improved example

--- examples/set_get.js
+++ examples/set_get.js
@@ -3,16 +3,33 @@ const Tile38 = require('tile38');
 const client = new Tile38();
 
 // set a coordinate
-client.set('fleet', 'truck', [33.5123, -112.2693]);
+client.set('fleet', 'truck1', [33.5123, -112.2693]);
+
+// set a coordinate, with completion handler and error handler
+client.set('fleet', 'truck2', [33.5211, -112.2710]).then(() => {
+    console.log("done");
+}).catch(err => {
+    console.error(err);
+});
+
 
 // retrieve it as geojson object:
-client.get('fleet', 'truck').then(obj => {
+client.get('fleet', 'truck1').then(obj => {
     // will print:  { object: { type: 'Point', coordinates: [ -112.2693, 33.5123 ] } }
     console.dir(obj);
 });
 
 // retrieve it as a simple point
-client.getPoint('fleet', 'truck').then(obj => {
+client.getPoint('fleet', 'truck2').then(obj => {
     // will print:  { point: { lat: 33.5123, lon: -112.2693 } }
     console.dir(obj);
+});
+
+
+// same as above but adds error handling:
+client.getPoint('fleet', 'truck2').then(obj => {
+    // will print:  { point: { lat: 33.5123, lon: -112.2693 } }
+    console.dir(obj);
+}).catch(err => {
+    console.error(err);
 });
