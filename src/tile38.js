6c9af7bcdc3ae94077a7bb75331f2eeff9d8bc55
Peter Hulst <peter.hulst@springshot.com>
2019-03-22 16:54:13 -0700

    removed Bluebird dependency

--- src/tile38.js
+++ src/tile38.js
@@ -1,6 +1,5 @@
 // const Redis = require('ioredis');
 const redis = require('redis');
-const Promise = require('bluebird');
 const Query = require('./tile38_query');
 const LiveGeofence = require('./live_geofence');
 
