99e97ffcb60c20bbed669a6ef3fca0b0e62a1fdc
Gie Spaepen <giespaepen@outlook.com>
2020-01-11 16:30:28 +0100

    Added Typescript, linting and build scripts

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        project: 'tsconfig.json',
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        // Special ESLint rules or overrides go here.
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/interface-name-prefix": [0, { "prefixWithI": "always" }],
+        "@typescript-eslint/no-use-before-define": ["error", { "functions": false }],
+        "eol-last": ["error", "always"],
+        "quotes": ["error", "double", { "allowTemplateLiterals": true }],
+    },
+}
